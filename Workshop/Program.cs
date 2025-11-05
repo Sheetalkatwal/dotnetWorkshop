d632318a93ecc431dcd08b897b7aac8e7db6a9bf
Sheetalkatwal <sheetalkatwal97@gmail.com>
2025-11-05 12:17:26 +0545

    Add project files.

---
+++ Workshop/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace workshop
+{
+    //class Program
+    //{
+    //    // TASK 1
+    //    static void Main(string[] args)
+    //    {
+    //        // Declare variable to store name
+    //        string userName = "Sital";
+
+    //        // Declare variable to store lucky number
+    //        int luckyNumber = 10;
+
+    //        // Print using string interpolation
+    //        Console.WriteLine($"Hello, {userName}! Your lucky number is {luckyNumber}.");
+    //    }
+    //}
+}
