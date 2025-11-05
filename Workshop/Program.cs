e4b217bdcd92b36fb8a0e3a6c2545a6549192cd7
Sheetalkatwal <sheetalkatwal97@gmail.com>
2025-11-05 21:37:34 +0545

    Task 6 completed.

--- Workshop/Program.cs
+++ Workshop/Program.cs
@@ -136,6 +136,45 @@
     //    }
     //}
 
+    //TASK 6
+
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+
+    //        List<string> fruits = new List<string> { "Apple", "Orange", "Grapes" };
+
+
+    //        fruits.Add("Mango");
+
+
+    //        fruits.Remove("Orange");
+
+    //        Console.WriteLine("Fruits in the list:");
+    //        foreach (string fruit in fruits)
+    //        {
+    //            Console.WriteLine(fruit);
+    //        }
+
+    //        Dictionary<int, string> fruitDictionary = new Dictionary<int, string>
+    //        {
+    //            {1, "Apple"},
+    //            {2, "Orange"},
+    //            {3, "Grapes"}
+    //        };
+
+
+    //        fruitDictionary.Add(4, "Mango");
+
+    //        Console.WriteLine("\nFruits in the dictionary:");
+    //        foreach (KeyValuePair<int, string> entry in fruitDictionary)
+    //        {
+    //            Console.WriteLine($"ID: {entry.Key}, Name: {entry.Value}");
+    //        }
+    //    }
+    //}
+
 
 
 
