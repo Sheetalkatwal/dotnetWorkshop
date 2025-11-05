769cbf4ed005a3aa315882cdb1ecde3882440408
Sheetalkatwal <sheetalkatwal97@gmail.com>
2025-11-05 21:03:00 +0545

    Task 4 completed.

--- Workshop/Program.cs
+++ Workshop/Program.cs
@@ -84,5 +84,39 @@
     //}
 
 
+    //TASK 4
+
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        int[] favoriteNumbers = { 8, 21, 6, 25, 9 };
+
+    //        Array.Sort(favoriteNumbers);
+
+    //        Array.Reverse(favoriteNumbers);
+
+    //        Console.WriteLine("Numbers in descending order:");
+    //        for (int i = 0; i < favoriteNumbers.Length; i++)
+    //        {
+    //            Console.WriteLine(favoriteNumbers[i]);
+    //        }
+
+    //        int position = Array.IndexOf(favoriteNumbers, 25);
+    //        if (position != -1)
+    //        {
+    //            Console.WriteLine($"The number 25 is at position {position} in the array.");
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("The number 25 is not in the array.");
+    //        }
+    //    }
+    //}
+
+
+
+
+
 }
 
