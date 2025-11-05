81c6ebd7f35f431a3741a7ebca9c96afeeb6c964
Sheetalkatwal <sheetalkatwal97@gmail.com>
2025-11-05 21:25:03 +0545

    Task 5 completed

--- Workshop/Program.cs
+++ Workshop/Program.cs
@@ -114,6 +114,28 @@
     //    }
     //}
 
+    //TASK 5
+
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        DateTime birthDate = new DateTime(2005, 4, 29);
+
+    //        DateTime currentDate = DateTime.Now;
+
+    //        TimeSpan ageSpan = currentDate - birthDate;
+    //        int ageInYears = (int)(ageSpan.TotalDays / 365.25);
+
+    //        Console.WriteLine($"Birthdate: {birthDate.ToShortDateString()}");
+    //        Console.WriteLine($"Current Date: {currentDate.ToShortDateString()}");
+    //        Console.WriteLine($"Age: {ageInYears} years");
+
+    //        DateTime newDate = birthDate.AddDays(10);
+    //        Console.WriteLine($"Birthdate + 10 days: {newDate.ToShortDateString()}");
+    //    }
+    //}
+
 
 
 
